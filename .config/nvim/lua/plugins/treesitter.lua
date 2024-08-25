95a0a268ad4d0fefd190bad3fb024837871b61a1
Rahul Kumar <xyz.rahulkumar2002@gmail.com>
2024-08-25 20:14:15 +0530

    added: top sticky context bar

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,36 +1,71 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		--init = function()
-		--end,
-		cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+		cmd = {
+			"TSInstall",
+			"TSBufEnable",
+			"TSBufDisable",
+			"TSModuleInfo",
+			"TSContextEnable",
+			"TSContextDisable",
+			"TSContextToggle",
+		},
 		build = ":TSUpdate",
-		opts = function()
-			local options = {
+		config = function()
+			require("nvim-treesitter.configs").setup({
 				ensure_installed = {
 					"html",
 					"lua",
 					"javascript",
-					"jsdoc",
 					"typescript",
-					"sql",
 					"java",
 					"comment",
 					"python",
+					"vimdoc",
+					"c",
+					"lua",
+					"rust",
+					"bash",
 				},
 
-				highlight = {
-					enable = true,
-					use_languagetree = true,
-				},
+				enable = true,
+				-- Install parsers synchronously (only applied to `ensure_installed`)
+				sync_install = false,
+				-- Automatically install missing parsers when entering buffer
+				-- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
+				auto_install = true,
 
 				indent = { enable = true },
-			}
 
-			return options
+				highlight = {
+					-- `false` will disable the whole extension
+					enable = true,
+
+					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+					-- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
+					-- Using this option may slow down your editor, and you may see some duplicate highlights.
+					-- Instead of true it can also be a list of languages
+					additional_vim_regex_highlighting = { "markdown" },
+					use_languagetree = true,
+				},
+			})
 		end,
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		config = function()
+			require("treesitter-context").setup({
+				enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+				-- Separator between context and content. Should be a single character string, like '-'.
+				-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+				separator = "─",
+			})
+
+			vim.cmd([[
+                    highlight TreesitterContext guibg=#2E3440 
+                    highlight TreesitterContextLineNumberBottom gui=underline guisp=Grey
+            ]])
 		end,
 	},
 	{
