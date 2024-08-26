20f9b4ed963f49bf40ad8b35dc6423ef9a4c766b
Rahul Kumar <xyz.rahulkumar2002@gmail.com>
2024-08-26 11:34:14 +0530

    updated: lualine UI

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -90,43 +90,13 @@ return {
 			padding = { left = 0, right = 1 }, -- We don't need space before this
 		})
 
-		ins_left({
-			-- mode component
-			"mode",
-			color = function()
-				-- auto change color according to neovims mode
-				local mode_color = {
-					n = colors.red,
-					i = colors.green,
-					v = colors.blue,
-					[""] = colors.blue,
-					V = colors.blue,
-					c = colors.magenta,
-					no = colors.red,
-					s = colors.orange,
-					S = colors.orange,
-					[""] = colors.orange,
-					ic = colors.yellow,
-					R = colors.violet,
-					Rv = colors.violet,
-					cv = colors.red,
-					ce = colors.red,
-					r = colors.cyan,
-					rm = colors.cyan,
-					["r?"] = colors.cyan,
-					["!"] = colors.red,
-					t = colors.red,
-				}
-				return { fg = mode_color[vim.fn.mode()] }
-			end,
-			padding = { right = 1 },
-		})
 
 		ins_left({
-			-- filesize component
-			"filesize",
-			cond = conditions.buffer_not_empty,
+			"branch",
+			icon = "",
+			color = { fg = colors.violet, gui = "bold" },
 		})
+
 
 		ins_left({
 			"filename",
@@ -134,81 +104,49 @@ return {
 			color = { fg = colors.magenta, gui = "bold" },
 		})
 
-		ins_left({ "location" })
 
-		ins_left({ "progress", color = { fg = colors.fg, gui = "bold" } })
 
 		ins_left({
-			"diagnostics",
-			sources = { "nvim_diagnostic" },
-			symbols = { error = " ", warn = " ", info = " " },
-			diagnostics_color = {
-				color_error = { fg = colors.red },
-				color_warn = { fg = colors.yellow },
-				color_info = { fg = colors.cyan },
-			},
-		})
-
-		-- Insert mid section. You can make any number of sections in neovim :)
-		-- for lualine it's any number greater then 2
-		ins_left({
-			function()
-				return "%="
-			end,
-		})
-
-		-- ins_left({
-		-- 	-- Lsp server name .
-		-- 	function()
-		-- 		local msg = "No Active Lsp"
-		-- 		local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-		-- 		local clients = vim.lsp.get_active_clients()
-		-- 		if next(clients) == nil then
-		-- 			return msg
-		-- 		end
-		-- 		for _, client in ipairs(clients) do
-		-- 			local filetypes = client.config.filetypes
-		-- 			if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-		-- 				return client.name
-		-- 			end
-		-- 		end
-		-- 		return msg
-		-- 	end,
-		-- 	icon = " LSP:",
-		-- 	color = { fg = "#ffffff", gui = "bold" },
-		-- })
-
-		-- Add components to right sections
-		ins_right({
-			"o:encoding", -- option component same as &encoding in viml
-			fmt = string.upper, -- I'm not sure why it's upper case either ;)
-			cond = conditions.hide_in_width,
-			color = { fg = colors.green, gui = "bold" },
-		})
-
-		ins_right({
-			"fileformat",
-			fmt = string.upper,
-			icons_enabled = false, -- I think icons are cool but Eviline doesn't have them. sigh
-			color = { fg = colors.green, gui = "bold" },
-		})
-
-		ins_right({
-			"branch",
-			icon = "",
-			color = { fg = colors.violet, gui = "bold" },
-		})
-
-		ins_right({
 			"diff",
 			-- Is it me or the symbol for modified us really weird
-			symbols = { added = " ", modified = "󰝤 ", removed = " " },
+			symbols = { added = "+", modified = "~", removed = "-" },
 			diff_color = {
 				added = { fg = colors.green },
 				modified = { fg = colors.orange },
 				removed = { fg = colors.red },
 			},
 			cond = conditions.hide_in_width,
+		})
+
+		ins_left({
+			function()
+				return "%="
+			end,
+		})
+
+
+		-- Add components to right sections
+		ins_right({
+			"diagnostics",
+			sources = { "nvim_diagnostic" },
+			symbols = { error = "E ", warn = "W ", info = "I " },
+			diagnostics_color = {
+				color_error = { fg = colors.red },
+				color_warn = { fg = colors.yellow },
+				color_info = { fg = colors.cyan },
+			},
+		})
+		ins_right({
+			-- filesize component
+			"filesize",
+			cond = conditions.buffer_not_empty,
+		})
+		ins_right({ "progress", color = { fg = colors.fg, gui = "bold" } })
+		ins_right({
+			"o:encoding", -- option component same as &encoding in viml
+			fmt = string.upper, -- I'm not sure why it's upper case either ;)
+			cond = conditions.hide_in_width,
+			color = { fg = colors.green, gui = "bold" },
 		})
 
 		ins_right({
