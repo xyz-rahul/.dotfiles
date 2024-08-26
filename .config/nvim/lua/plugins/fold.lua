8d2d697b8a46e012cbfb045550340a22c42cbf65
Rahul Kumar <xyz.rahulkumar2002@gmail.com>
2024-08-26 09:18:55 +0530

    updated: fold keybinding

--- .config/nvim/lua/plugins/fold.lua
+++ .config/nvim/lua/plugins/fold.lua
@@ -7,17 +7,14 @@ return {
 		vim.o.foldlevelstart = 99
 		vim.o.foldenable = true
 
-		-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-		vim.keymap.set("n", "zO", require("ufo").openAllFolds)
-		vim.keymap.set("n", "zC", require("ufo").closeAllFolds)
-        vim.keymap.set('n', 'K', function()
-    local winid = require('ufo').peekFoldedLinesUnderCursor()
-    if not winid then
-        -- choose one of coc.nvim and nvim lsp
-        vim.fn.CocActionAsync('definitionHover') -- coc.nvim
-        vim.lsp.buf.hover()
-    end
-end)
+		vim.keymap.set("n", "K", function()
+			local winid = require("ufo").peekFoldedLinesUnderCursor()
+			if not winid then
+				-- choose one of coc.nvim and nvim lsp
+				vim.fn.CocActionAsync("definitionHover") -- coc.nvim
+				vim.lsp.buf.hover()
+			end
+		end)
 
 		require("ufo").setup()
 	end,
