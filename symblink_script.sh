e2e69f9c989c1234b0e453421b1d687373345a08
Rahul Kumar <rahulkumar@Rahuls-MacBook-Air.local>
2024-11-12 22:20:20 +0530

    added script to add dotfile symblink

--- symblink_script.sh
+++ symblink_script.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
-ln -s $(pwd)/.bash_profile ~/.bash_profile
-ln -s $(pwd)/.bashrc ~/.bashrc
-ln -s $(pwd)/.dircolors ~/.dircolors
-ln -s $(pwd)/.ideavimrc ~/.ideavimrc
-ln -s $(pwd)/.local ~/.local
-ln -s $(pwd)/.skhdrc ~/.skhdrc
-ln -s $(pwd)/.vimrc ~/.vimrc
-ln -s $(pwd)/.yabairc ~/.yabairc
-ln -s $(pwd)/karabiner.json ~/.config/karabiner/karabiner.json
-ln -s $(pwd)/notes ~/notes
-ln -s $(pwd)/nvim ~/nvim
-ln -s $(pwd)/.tmux.conf ~/.tmux.conf
-ln -s $(pwd)/waifu ~/waifu
-ln -s $(pwd)/wezterm ~/.config/wezterm
+# Function to create symbolic links with confirmation
+create_symlink() {
+    local target=$1
+    local link_name=$2
+
+    # Check if the target link_name already exists
+    if [ -e "$link_name" ] || [ -L "$link_name" ]; then
+        read -p "$link_name already exists. Do you want to overwrite it? (y/n): " choice
+        if [[ "$choice" =~ ^[Yy]$ ]]; then
+            echo "Removing existing file/link: $link_name"
+            rm -f "$link_name"
+        else
+            echo "Skipping: $link_name"
+            return 1  # Skip if user does not want to overwrite
+        fi
+    fi
+
+    # Create the symlink if not skipped
+    ln -s "$target" "$link_name"
+    echo "Created symlink: $link_name -> $target"
+}
+
+# Creating the symbolic links
+create_symlink "$(pwd)/.bash_profile" ~/.bash_profile
+create_symlink "$(pwd)/.bashrc" ~/.bashrc
+create_symlink "$(pwd)/.dircolors" ~/.dircolors
+create_symlink "$(pwd)/.ideavimrc" ~/.ideavimrc
+create_symlink "$(pwd)/.local/bin" ~/.local/bin
+create_symlink "$(pwd)/.skhdrc" ~/.skhdrc
+create_symlink "$(pwd)/.vimrc" ~/.vimrc
+create_symlink "$(pwd)/.yabairc" ~/.yabairc
+create_symlink "$(pwd)/karabiner.json" ~/.config/karabiner/karabiner.json
+create_symlink "$(pwd)/notes" ~/notes
+create_symlink "$(pwd)/nvim" ~/.config/nvim
+create_symlink "$(pwd)/.tmux.conf" ~/.tmux.conf
+create_symlink "$(pwd)/waifu" ~/waifu
+create_symlink "$(pwd)/wezterm" ~/.config/wezterm
 
