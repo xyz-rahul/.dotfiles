dcd9328b42e1fb897216477509d2658887637c6b
Rahul Kumar <rahulkumar@Rahuls-MacBook-Air.local>
2024-12-10 12:59:23 +0530

    updated: treesitter to auto install parsers

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,17 +4,9 @@ return {
         build = ":TSUpdate",
         config = function()
             require("nvim-treesitter.configs").setup({
-                ensure_installed = { "bash", "c", "cpp", "css", "dockerfile", "go", "graphql", "haskell", 'html', 'http', "java", "javascript", "typescript", "jq", "jsdoc", "json", "kotlin", "lua", "nginx", "php", "prisma", "python", "rust", "yaml" },
-                enable = true,
-                -- Install parsers synchronously (only applied to `ensure_installed`)
-                sync_install = false,
                 -- Automatically install missing parsers when entering buffer
                 -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-                indent = { enable = true },
-                highlight = {
-                    -- `false` will disable the whole extension
-                    enable = true,
-                },
+                auto_install = true,
             })
         end,
     },
