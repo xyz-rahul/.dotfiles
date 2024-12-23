c5c387569cc98f70f9eb85eff595b3f6ba3a4b1b
Rahul Kumar <rahulkumar@Rahuls-MacBook-Air.local>
2024-12-23 16:34:50 +0530

    fix lsp server showing at start up

--- nvim/init.lua
+++ nvim/init.lua
@@ -35,5 +35,5 @@ require("lazy").setup({
     { import = "plugins" },
   },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = true,notify = false  },
 })
