494dd3daf27dbe74b8c4b831a9486b967e8c25e0
Rahul Kumar <xyz.rahulkumar2002@gmail.com>
2025-01-04 18:04:35 +0530

    just some randowm changes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,31 +1,53 @@
 local wezterm = require("wezterm")
-local mytable = require("mystdlib").mytable
-
-local wallpapers_list = wezterm.glob(wezterm.home_dir .. "/waifu/*")
-local Wallpaper = require("wallpaper"):init(wallpapers_list)
-
 local config = wezterm.config_builder()
 
--- config.background = Wallpaper:background()
+-- OPACITY SETTINGS
+local opacity = 0.8
+config.window_background_opacity = opacity
 
------------------ OPACITY ----------------
-local opacities = { 1, 0.75, 0.5, 0.25 }
-local opacity_index = 1
+-- toggle function
 wezterm.on("toggle-opacity", function(window, pane)
 	local overrides = window:get_config_overrides() or {}
-	if overrides.window_background_opacity then
-		opacity_index = opacity_index % #opacities + 1
-		overrides.window_background_opacity = opacities[opacity_index]
+	if not overrides.window_background_opacity then
+		-- if no override is setup, override the default opacity value with 1.0
+		overrides.window_background_opacity = 1.0
 	else
-		overrides.window_background_opacity = opacities[1]
+		-- if there is an override, make it nil so the opacity goes back to the default
+		overrides.window_background_opacity = nil
 	end
 	window:set_config_overrides(overrides)
 end)
 
------------------ CONFIG ----------------
--- config.debug_key_events = true
+-- FONT SETTINGS
+config.font_size = 16
+config.font = wezterm.font_with_fallback({
+	{ family = "JetBrains Mono", weight = "Medium" },
+	{ family = "Terminus", weight = "Bold" },
+	"Noto Color Emoji",
+})
 
+-- COLOR SCHEME
+config.color_scheme = "midnight-in-mojave"
+
+-- TAB BAR CONFIGURATION
+config.hide_tab_bar_if_only_one_tab = true
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = false
+config.show_tab_index_in_tab_bar = false
+config.switch_to_last_active_tab_when_closing_tab = true
+
+-- WINDOW CONFIGURATION
+config.window_close_confirmation = "NeverPrompt"
+config.window_frame = {
+	active_titlebar_bg = "#090909",
+}
+
+-- SCROLLBAR
+config.enable_scroll_bar = true
+
+-- KEY BINDINGS
 config.keys = {
+	-- Toggle Fullscreen
 	{
 		key = "Enter",
 		mods = "CMD",
@@ -36,15 +58,9 @@ config.keys = {
 		mods = "CMD",
 		action = wezterm.action.EmitEvent("toggle-opacity"),
 	},
-	-- copy/paste --
+	-- Copy/Paste
 	{ key = "c", mods = "CTRL|SHIFT", action = wezterm.action.CopyTo("Clipboard") },
 	{ key = "v", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("Clipboard") },
 }
 
-local full_config = mytable.merge_all(
-	require("appearance"),
-	config,
-	{} -- so the last table can have an ending comma for git diffs :)
-)
-return full_config
--- for k,v in pairs(second_table) do first_table[k] = v end
+return config
