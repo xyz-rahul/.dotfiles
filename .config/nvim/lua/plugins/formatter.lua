2d0626b950f2985eeee6654027fa23a9a6abac6b
Rahul Kumar <rahul.kumar2@cashify.in>
2025-04-19 14:34:57 +0530

    fix autocomplete formatter

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -1,25 +1,38 @@
 return {
-	"stevearc/conform.nvim",
-	init = function()
-		vim.o.formatexpr = [[v:lua.require("conform").formatexpr()]]
-	end,
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				python = { "isort", "black" },
-				javascript = { "prettierd", "prettier", stop_after_first = true },
-				typescript = { "prettierd", "prettier", stop_after_first = true },
-				html = { "prettierd", "prettier", stop_after_first = true },
-				css = { "prettierd", "prettier", stop_after_first = true },
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim", -- optional for ensuring tools
+		opts = {
+			ensure_installed = {
+				"stylua",
+				"prettier",
+				"prettierd",
+				"black",
 			},
-		})
-		vim.keymap.set({ "n", "v" }, "<leader>fm", function()
-			require("conform").format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
+		},
+	},
+	{
+		"stevearc/conform.nvim",
+		init = function()
+			vim.o.formatexpr = [[v:lua.require("conform").formatexpr()]]
+		end,
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					python = { "isort", "black" },
+					javascript = { "prettierd", "prettier", stop_after_first = true },
+					typescript = { "prettierd", "prettier", stop_after_first = true },
+					html = { "prettierd", "prettier", stop_after_first = true },
+					css = { "prettierd", "prettier", stop_after_first = true },
+				},
 			})
-		end, { desc = "Format file or range (in visual mode)" })
-	end,
+			vim.keymap.set({ "n", "v" }, "<leader>fm", function()
+				require("conform").format({
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 1000,
+				})
+			end, { desc = "Format file or range (in visual mode)" })
+		end,
+	},
 }
